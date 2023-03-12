751add550e0c4fc38b4c5ffa2b755737f68c4127
vib1910601 <vib1910601@student.ctu.edu.vn>
2023-03-12 08:13:01 -0800

    Tao trang them lien he

--- src/router/index.js
+++ src/router/index.js
@@ -14,9 +14,14 @@ const routes = [
   {
     path: "/contacts/:id",
     name: "contact.edit",
-    component: () => import("@/views/ContactEdit.vue"),
+    component: () => import("../views/ContactEdit.vue"),
     props: true, // Truyền các biến trong $route.params vào làm props
   },
+  { 
+    path: "/contacts",
+    name: "contact.add",
+    component: () => import("../views/ContactAdd.vue"),props: true 
+    }
 ];
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
