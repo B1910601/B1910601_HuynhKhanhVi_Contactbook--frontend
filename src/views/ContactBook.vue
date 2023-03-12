96cf4570ac35e91ef24a41c5296df7e6a0fa80fa
vib1910601 <vib1910601@student.ctu.edu.vn>
2023-03-12 08:02:55 -0800

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -33,6 +33,16 @@
           <i class="fas fa-address-card"></i>
         </h4>
         <ContactCard :contact="activeContact" />
+        <router-link
+          :to="{
+            name: 'contact.edit',
+            params: { id: activeContact._id },
+          }"
+        >
+          <span class="mt-2 badge badge-warning">
+            <i class="fas fa-edit"></i> Hiệu chỉnh</span
+          >
+        </router-link>
       </div>
     </div>
   </div>
@@ -115,9 +125,7 @@ export default {
     this.refreshList();
   },
 };
-
 </script>
-
 
 <style scoped>
 .page {
@@ -125,4 +133,3 @@ export default {
   max-width: 750px;
 }
 </style>
-
